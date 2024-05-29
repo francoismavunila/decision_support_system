e7ee265a37f39e4ffd7de992b1dcafaa39c36d12
francoismavunila <mavunila002@gmail.com>
2024-05-29 04:23:13 +0000

    refined predictions

--- dss/urls.py
+++ dss/urls.py
@@ -1,7 +1,8 @@
 # predictions/urls.py
 from django.urls import path
-from .views import PredictSales
+from .views import PredictSales, ProductInsights
 
 urlpatterns = [
     path('', PredictSales.as_view(), name='predict-sales'),
+    path('<int:product_id>/', ProductInsights.as_view(), name='product-detail'),
 ]
